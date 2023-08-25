e8363202a03bea539a052aa8028c9829872a1e9c
Макс Мацкевич <118908699+MaksMeamk@users.noreply.github.com>
2023-08-25 11:47:53 +0000

    Start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Autorization/>}/>
+          <Route path="/registration" element={<Registration/>}/>
+          <Route path="/tasks" element={<Tasks/>}/>
+        </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
