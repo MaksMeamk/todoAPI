61f7252e09810f900e741d258f91aa95461687f5
Макс Мацкевич <118908699+MaksMeamk@users.noreply.github.com>
2023-10-02 10:53:10 +0000

    validation ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Select } from "antd";
+import { useState } from "react";
 
 function App() {
+  const [pass, setPass] = useState("");
+  const gender = [
+    { value: "male", label: "male" },
+    { value: "female", label: "female" },
+  ];
   return (
     <div className="App">
       <Form
@@ -19,7 +25,6 @@ function App() {
         }}
         onFinish={console.log("Good finish")}
         onFinishFailed={console.log("Error")}
-        autoComplete="off"
       >
         <Form.Item
           label="Username"
@@ -33,6 +38,22 @@ function App() {
         >
           <Input />
         </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+            {
+              type: "email",
+              message: "Enter a valid email address",
+            },
+          ]}
+        >
+          <Input type={"email"} />
+        </Form.Item>
 
         <Form.Item
           label="Password"
@@ -42,19 +63,80 @@ function App() {
               required: true,
               message: "Please input your password!",
             },
+            { min: 6, message: "Min length - 6 symbols" },
+            {
+              validator: (rule, value, callback) => {
+                if (!/[A-ZА-Я]/.test(value)) {
+                  callback(
+                    "The field must contain at least one capital letter",
+                  );
+                } else {
+                  callback();
+                }
+              },
+            },
+          ]}
+        >
+          <Input.Password onChange={(e) => setPass(e.target.value)} />
+        </Form.Item>
+        <Form.Item
+          label="Сonfirm password"
+          name="confirmPassword"
+          rules={[
+            {
+              required: true,
+              message: "Please input your confirm password!",
+            },
+            {
+              validator: (rule, value, callback) => {
+                if (!(pass === value)) {
+                  callback("Password fields must be identical");
+                } else {
+                  callback();
+                }
+              },
+            },
           ]}
         >
           <Input.Password />
         </Form.Item>
+        <Form.Item
+          label="Date of Birth"
+          name="dateOfBirth"
+          rules={[
+            {
+              required: true,
+              message: "Please select your date of birth!",
+            },
+          ]}
+        >
+          <Input type={"date"} />
+        </Form.Item>
+        <Form.Item
+          label="Gender"
+          name="gender"
+          rules={[
+            {
+              required: true,
+              message: "Please select your gender",
+            },
+          ]}
+        >
+          <Select options={gender} />
+        </Form.Item>
 
         <Form.Item
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        ></Form.Item>
+          label="Phone number"
+          name="phoneNumber"
+          rules={[
+            {
+              required: true,
+              message: "Please input your phone number",
+            },
+          ]}
+        >
+          <Input type={"number"} />
+        </Form.Item>
 
         <Form.Item
           wrapperCol={{
@@ -63,7 +145,7 @@ function App() {
           }}
         >
           <Button type="primary" htmlType="submit">
-            Submit
+            Registration
           </Button>
         </Form.Item>
       </Form>
